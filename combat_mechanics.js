d430354c59587cb0d88008285dfe8dace3ac761e
Vadim Zgonnik <vadimzgonnik95@gmail.com>
2018-02-08 13:39:53 +0200

    modified the creature creation mechanism

---
+++ combat_mechanics.js
@@ -0,0 +1,15 @@
+var cardDecks = [];
+
+function randomaizer(){  // рандомайзер делает рандомные существа(объекты) с уроном и защитой от 0 до 50
+	
+	for(i=0; i<5; i++){
+		var rand = {damage: parseInt(Math.random()*50), armor: parseInt(Math.random()*50)};
+		cardDecks.push(rand);
+	}
+	return cardDecks;
+}
+
+
+console.log(randomaizer());
+
+console.log(cardDecks)
