c2e7f3ce19089465d6ccd26f808c3123a4694374
Vadim Zgonnik <vadimzgonnik95@gmail.com>
2018-02-08 13:53:26 +0200

    nothing happened

--- combat_mechanics.js
+++ combat_mechanics.js
@@ -12,4 +12,7 @@ function randomaizer(){  // рандомайзер делает рандомны
 
 console.log(randomaizer());
 
-console.log(cardDecks)
+console.log(cardDecks);
+
+
+//////////////////////fight////////////////////////////
